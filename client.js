e1a6e036e441724bc2d5c276b44e96dd0dc8d9ce
Alexis <alexisko@hawaii.edu>
2017-07-08 12:27:58 -1000

    Cleaned up code and added comments

--- client.js
+++ client.js
@@ -5,20 +5,24 @@ var uri, showHeader = false;
 const client = new net.Socket();
 client.setEncoding('utf8');
 
+//CHECK IF USER WANTS TO ONLY DISPLAY HEADERS
 if(process.argv[3] === "-H") {
   showHeader = true;
 }
 
+//CONNECT TO LOCAL HOST
 if(process.argv[2] === "localhost") {
   uri = process.argv[2];
   client.connect(8080, uri, () => {
     client.write(formatHeader(uri));
   });
+//CONNECT TO OTHER
 } else if(process.argv[2] !== undefined) {
   uri = process.argv[2];
   client.connect(80, uri, () => {
     client.write(formatHeader(uri));
   });
+//NO URI SPECIFIED
 } else {
   //HELP MESSAGE
   process.stdout.write("--- HELP/USAGE ---\n");
@@ -28,56 +32,6 @@ if(process.argv[2] === "localhost") {
   process.stdout.write("If you would only like to display headers in the output, run in this format:\n");
   process.stdout.write("* example: node client.js localhost -H\n");
 }
-
-// if(process.argv[2] !== undefined && process.argv[3] === "-H") {
-//   var header;
-//   uri = process.argv[2];
-//   if(uri === 'localhost') {
-//     client.connect(8080, uri, () => {
-//       client.write(formatHeader(uri));
-//       client.on('data', (data) => {
-//         header = data.toString().substr(0, data.indexOf('<'));
-//         process.stdout.write(header);
-//       });
-//     });
-//   } else {
-//     client.connect(80, uri, () => {
-//       client.write(formatHeader(uri));
-//       client.on('data', (data) => {
-//         header = data.toString().substr(0, data.indexOf('<'));
-//         process.stdout.write(header);
-//       });
-//     });
-//   }
-// } else if(process.argv[2] !== undefined) {
-//   var body;
-//   uri = process.argv[2];
-//   if(uri === 'localhost') {
-//     client.connect(8080, uri, () => {
-//       client.write(formatHeader(uri));
-//       client.on('data', (data) => {
-//         body = data.toString().substr(data.indexOf('<'), data.length);
-//         process.stdout.write(body);
-//       });
-//     });
-//   } else {
-//     client.connect(80, uri, () => {
-//       client.write(formatHeader(uri));
-//       client.on('data', (data) => {
-//         body = data.toString().substr(data.indexOf('<'), data.length);
-//         process.stdout.write(body);
-//       });
-//     });
-//   }
-// } else {
-//   //HELP MESSAGE
-//   process.stdout.write("--- HELP/USAGE ---\n");
-//   process.stdout.write("In order to properly run this HTTP socket client,\nyou must specify the host and uri to request a resource from.\n");
-//   process.stdout.write("* example: node client.js www.devleague.com\n\n");
-//   process.stdout.write("--- OPTIONS ---\n");
-//   process.stdout.write("If you would only like to display headers in the output, run in this format:\n");
-//   process.stdout.write("* example: node client.js localhost -H\n");
-// }
 
 client.on('data', (data) => {
   var header, body;
@@ -90,6 +44,7 @@ client.on('data', (data) => {
   }
 });
 
+//REQUEST HEADER
 function formatHeader(URI) {
   var method, host, accept, date, userAgent;
   method = "GET / HTTP/1.1\r\n";
