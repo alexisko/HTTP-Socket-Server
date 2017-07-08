afe9167928ff74b4caa3eeb8f442770479f4ba97
Alexis <alexisko@hawaii.edu>
2017-07-08 12:26:14 -1000

    Refactored code

--- client.js
+++ client.js
@@ -1,50 +1,24 @@
 /*jshint esversion: 6 */
 const net = require('net');
 
-var uri;
+var uri, showHeader = false;
 const client = new net.Socket();
 client.setEncoding('utf8');
 
-if(process.argv[2] !== undefined && process.argv[3] === "-H") {
-  var header;
+if(process.argv[3] === "-H") {
+  showHeader = true;
+}
+
+if(process.argv[2] === "localhost") {
   uri = process.argv[2];
-  if(uri === 'localhost') {
-    client.connect(8080, uri, () => {
-      client.write(formatHeader(uri));
-      client.on('data', (data) => {
-        header = data.toString().substr(0, data.indexOf('<'));
-        process.stdout.write(header);
-      });
-    });
-  } else {
-    client.connect(80, uri, () => {
-      client.write(formatHeader(uri));
-      client.on('data', (data) => {
-        header = data.toString().substr(0, data.indexOf('<'));
-        process.stdout.write(header);
-      });
-    });
-  }
+  client.connect(8080, uri, () => {
+    client.write(formatHeader(uri));
+  });
 } else if(process.argv[2] !== undefined) {
-  var body;
   uri = process.argv[2];
-  if(uri === 'localhost') {
-    client.connect(8080, uri, () => {
-      client.write(formatHeader(uri));
-      client.on('data', (data) => {
-        body = data.toString().substr(data.indexOf('<'), data.length);
-        process.stdout.write(body);
-      });
-    });
-  } else {
-    client.connect(80, uri, () => {
-      client.write(formatHeader(uri));
-      client.on('data', (data) => {
-        body = data.toString().substr(data.indexOf('<'), data.length);
-        process.stdout.write(body);
-      });
-    });
-  }
+  client.connect(80, uri, () => {
+    client.write(formatHeader(uri));
+  });
 } else {
   //HELP MESSAGE
   process.stdout.write("--- HELP/USAGE ---\n");
@@ -54,6 +28,67 @@ if(process.argv[2] !== undefined && process.argv[3] === "-H") {
   process.stdout.write("If you would only like to display headers in the output, run in this format:\n");
   process.stdout.write("* example: node client.js localhost -H\n");
 }
+
+// if(process.argv[2] !== undefined && process.argv[3] === "-H") {
+//   var header;
+//   uri = process.argv[2];
+//   if(uri === 'localhost') {
+//     client.connect(8080, uri, () => {
+//       client.write(formatHeader(uri));
+//       client.on('data', (data) => {
+//         header = data.toString().substr(0, data.indexOf('<'));
+//         process.stdout.write(header);
+//       });
+//     });
+//   } else {
+//     client.connect(80, uri, () => {
+//       client.write(formatHeader(uri));
+//       client.on('data', (data) => {
+//         header = data.toString().substr(0, data.indexOf('<'));
+//         process.stdout.write(header);
+//       });
+//     });
+//   }
+// } else if(process.argv[2] !== undefined) {
+//   var body;
+//   uri = process.argv[2];
+//   if(uri === 'localhost') {
+//     client.connect(8080, uri, () => {
+//       client.write(formatHeader(uri));
+//       client.on('data', (data) => {
+//         body = data.toString().substr(data.indexOf('<'), data.length);
+//         process.stdout.write(body);
+//       });
+//     });
+//   } else {
+//     client.connect(80, uri, () => {
+//       client.write(formatHeader(uri));
+//       client.on('data', (data) => {
+//         body = data.toString().substr(data.indexOf('<'), data.length);
+//         process.stdout.write(body);
+//       });
+//     });
+//   }
+// } else {
+//   //HELP MESSAGE
+//   process.stdout.write("--- HELP/USAGE ---\n");
+//   process.stdout.write("In order to properly run this HTTP socket client,\nyou must specify the host and uri to request a resource from.\n");
+//   process.stdout.write("* example: node client.js www.devleague.com\n\n");
+//   process.stdout.write("--- OPTIONS ---\n");
+//   process.stdout.write("If you would only like to display headers in the output, run in this format:\n");
+//   process.stdout.write("* example: node client.js localhost -H\n");
+// }
+
+client.on('data', (data) => {
+  var header, body;
+  header = data.toString().substr(0, data.indexOf('<'));
+  body = data.toString().substr(data.indexOf('<'), data.length);
+  if(showHeader === true) {
+    process.stdout.write(header);
+  } else {
+    process.stdout.write(body);
+  }
+});
 
 function formatHeader(URI) {
   var method, host, accept, date, userAgent;
